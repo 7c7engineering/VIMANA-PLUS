ab23a5f28f4997858b16e33331e6d6f2f18af6e2
Max Vandenbussche <max@7c7engineering.com>
2023-05-24 08:14:32 +0200

    Add fun network names to choose from

---
+++ Firmware/components/vimana_network/include/networknames.h
@@ -0,0 +1,24 @@
+#pragma once
+
+const char * networkNames[] = {
+    "SkyHighNet",
+    "AeroNet",
+    "JetWiFi",
+    "FlyFi",
+    "AirLink",
+    "PlaneNet",
+    "WingWire",
+    "AirWeb",
+    "GlideNet",
+    "AviaNet",
+    "CloudWiFi",
+    "SkyNet",
+    "AirNet",
+    "PlaneConnect",
+    "AeroLink",
+    "JetConnect",
+    "FlyNet",
+    "WingFi",
+    "AeroWeb",
+    "CloudConnect"
+};
